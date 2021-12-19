568e4c386346239f4acd605dd842e8e7d94d0086
Kwitsch <kwitsch@gmail.com>
2021-12-19 19:36:32 +0100

    main.go added

---
+++ src/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/kwitsch/docker-ArpRedisCollector/collector"
+	"github.com/kwitsch/docker-ArpRedisCollector/config"
+)
+
+func main() {
+	cfg, cErr := config.Get()
+	if cErr == nil {
+		arp, aErr := collector.New(&cfg.Arp)
+		if aErr == nil {
+			fmt.Println("Collector start")
+			intChan := make(chan os.Signal, 1)
+			signal.Notify(intChan, os.Interrupt)
+			for {
+				select {
+				case a := <-arp.ArpChannel:
+					fmt.Println(a.String())
+				case <-intChan:
+					fmt.Println("Collector stopping")
+					arp.Close()
+					os.Exit(0)
+				}
+			}
+		} else {
+			fmt.Println(aErr)
+			os.Exit(2)
+		}
+	} else {
+		fmt.Println(cErr)
+		os.Exit(1)
+	}
+}
