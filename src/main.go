beddfda4201f7603168d6b111d08902cfdd725b7
Kwitsch <kwitsch@gmail.com>
2021-12-23 00:04:37 +0100

    redis client added

--- src/main.go
+++ src/main.go
@@ -7,28 +7,35 @@ import (
 
 	"github.com/kwitsch/ArpRedisCollector/collector"
 	"github.com/kwitsch/ArpRedisCollector/config"
+	"github.com/kwitsch/ArpRedisCollector/redis"
 )
 
 func main() {
 	cfg, cErr := config.Get()
 	if cErr == nil {
-		arp, aErr := collector.New(&cfg.Arp)
-		if aErr == nil {
-			fmt.Println("Collector start")
-			intChan := make(chan os.Signal, 1)
-			signal.Notify(intChan, os.Interrupt)
-			for {
-				select {
-				case a := <-arp.ArpChannel:
-					fmt.Println(a.String())
-				case <-intChan:
-					fmt.Println("Collector stopping")
-					arp.Close()
-					os.Exit(0)
+		redis, rErr := redis.New(&cfg.Redis)
+		if rErr == nil {
+			arp, aErr := collector.New(&cfg.Arp)
+			if aErr == nil {
+				fmt.Println("Collector start")
+				intChan := make(chan os.Signal, 1)
+				signal.Notify(intChan, os.Interrupt)
+				for {
+					select {
+					case a := <-arp.ArpChannel:
+						redis.Publish(&a)
+					case <-intChan:
+						fmt.Println("Collector stopping")
+						arp.Close()
+						os.Exit(0)
+					}
 				}
+			} else {
+				fmt.Println(aErr)
+				os.Exit(3)
 			}
 		} else {
-			fmt.Println(aErr)
+			fmt.Println(rErr)
 			os.Exit(2)
 		}
 	} else {
---
+++ src/redis/redis.go
@@ -0,0 +1,50 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/irai/arp"
+	"github.com/kwitsch/ArpRedisCollector/config"
+)
+
+type Client struct {
+	config *config.RedisConfig
+	client *redis.Client
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func New(cfg *config.RedisConfig) (*Client, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:            cfg.Address,
+		Username:        cfg.Username,
+		Password:        cfg.Password,
+		DB:              cfg.Database,
+		MaxRetries:      cfg.ConnectionAttempts,
+		MaxRetryBackoff: time.Duration(cfg.ConnectionCooldown),
+	})
+	ctx, cancel := context.WithCancel(context.Background())
+
+	_, err := rdb.Ping(ctx).Result()
+	if err == nil {
+		res := &Client{
+			config: cfg,
+			client: rdb,
+			ctx:    ctx,
+			cancel: cancel,
+		}
+		return res, nil
+	}
+	cancel()
+	return nil, err
+}
+
+func (c *Client) Close() {
+	c.cancel()
+}
+
+func (c *Client) Publish(entry *arp.MACEntry) {
+
+}
