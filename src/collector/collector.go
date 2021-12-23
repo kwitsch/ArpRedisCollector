b8dc70c59cd9b7b9b6e54fdc774b8a543bd70ae8
Kwitsch <kwitsch@gmail.com>
2021-12-23 18:05:58 +0100

    moved network functions to seperate package

--- src/collector/collector.go
+++ src/collector/collector.go
@@ -1,18 +1,14 @@
 package collector
 
 import (
-	"bufio"
 	"context"
-	"encoding/binary"
 	"fmt"
 	"net"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/irai/arp"
 	"github.com/kwitsch/ArpRedisCollector/config"
+	arcnet "github.com/kwitsch/ArpRedisCollector/net"
 )
 
 const (
@@ -83,10 +79,10 @@ func (c *Collector) PublishTable() {
 func getConfig(cfg *config.ArpConfig) (*arp.Config, error) {
 	iface, err := net.InterfaceByName(cfg.Interface)
 	if err == nil {
-		homeNet := getHomeNet(iface)
+		homeNet := arcnet.GetHomeNet(iface)
 		if homeNet != nil {
 			var gateway net.IP
-			gateway, err = getDefaultGateway(cfg.Interface)
+			gateway, err = arcnet.GetDefaultGateway(cfg.Interface)
 			if err == nil {
 				res := &arp.Config{
 					NIC:                     iface.Name,
@@ -105,50 +101,4 @@ func getConfig(cfg *config.ArpConfig) (*arp.Config, error) {
 		}
 	}
 	return nil, err
-}
-
-func getHomeNet(iface *net.Interface) *net.IPNet {
-	addrs, ierr := iface.Addrs()
-	if ierr == nil {
-		for _, a := range addrs {
-			switch v := a.(type) {
-			case *net.IPNet:
-				if strings.Count(v.String(), ":") < 2 && !v.IP.IsLoopback() {
-					return v
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func getDefaultGateway(ifName string) (gw net.IP, err error) {
-
-	file, err := os.Open("/proc/net/route")
-	if err != nil {
-		return net.IPv4zero, err
-	}
-	defer file.Close()
-
-	res := net.IPv4zero
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		// 0 = Iface
-		// 1 = Destination
-		// 2 = Gateway
-		fields := strings.Split(scanner.Text(), "\t")
-		if fields[0] == ifName {
-			d64, _ := strconv.ParseInt("0x"+fields[2], 0, 64)
-			if d64 != 0 {
-				d32 := uint32(d64)
-				res = make(net.IP, 4)
-				binary.LittleEndian.PutUint32(res, d32)
-				break
-			}
-		}
-	}
-	if res.Equal(net.IPv4zero) {
-		err = fmt.Errorf("Coulden't get default gateway for interface %s", ifName)
-	}
-	return res, err
 }
