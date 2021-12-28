0b2c7a5758fa9cbee3a210209f408f7f01d58506
Kwitsch <kwitsch@gmail.com>
2021-12-28 17:09:51 +0100

    changed defaults

--- src/config/config.go
+++ src/config/config.go
@@ -27,7 +27,7 @@ type RedisConfig struct {
 
 type ArpConfig struct {
 	CIDRs         map[int]string `koanf:"subnet"`
-	PollIntervall time.Duration  `koan:"pollIntervall" default:"15m"`
+	PollIntervall time.Duration  `koan:"pollIntervall" default:"10m"`
 	Timeout       time.Duration  `koanf:"timeout" default:"200ms"`
 	StaticTable   bool           `koanf:"staticTable" default:"false"`
 	Subnets       []*net.IPNet
